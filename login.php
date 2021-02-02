080fa7e6c223eb9183ed033d21e263f9de9cd7fb
Willy Sambora <Willysambora002@gmail.com>
2021-02-02 12:04:55 +0300

    added login.php and main.css

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Index</title>
+	<h1>Login as:</h1>
+	<link rel="stylesheet" type="text/css" href="main.css">
+
+</head>
+<body>
+	
+<div class='container1' style='margin-top: 70px;'> 
+		<div class='col-md-4'>
+				<center>
+					<div class='img-login-icons'>
+						<img  class='img-responsive' src='images/admn.png' alt='login as a doctor' />
+							</div>
+								<center><a href="#admin.php"><div class='badge-header'>Admin</div></a></center> </center> 
+							</div> 
+							<div class='col-md-4'><center>
+								<div class='img-login-icons'>
+									<img  class='img-responsive' src='images/docy.png' alt='login as a doctor' />
+										</div>
+										<center><a href="#doctor.php"><div class='badge-header'>Doctor</div></a></center> 
+										</center>
+									</div> 
+									
+									<div class='col-md-4'> 
+
+										<center>
+											<div class='img-login-icons'>
+												<img  class='img-responsive' src='images/patient.png' alt='login as a doctor' />
+											</div>
+											<center><a href='#login-patient.php'><div class='badge-header'>Patient</div></a></center></center>
+		</div> 
+									
+</div> 
+
+
+</body>
+</html>
